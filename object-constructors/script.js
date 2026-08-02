d43d79793c8e4888fea9f8d5722e4dd681b18638
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-02 08:00:30 +0200

    Add Player object constructor example

--- object-constructors/script.js
+++ object-constructors/script.js
@@ -1,3 +1,19 @@
+function Player(name, marker) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function () {
+        console.log(this.name);
+    };
+}
+
+const player1 = new Player("steve", "X");
+const player2 = new Player("also steve", "O");
+player1.sayName(); // logs "steve"
+player2.sayName(); // logs "also steve"
+
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
