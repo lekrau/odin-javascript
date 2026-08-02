e3925f7779b35aba9119c707ed0ce5d7eb6c42f6
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-02 08:23:02 +0200

    Explore Player prototype chain

--- object-constructors/script.js
+++ object-constructors/script.js
@@ -13,3 +13,17 @@ const player1 = new Player("steve", "X");
 const player2 = new Player("also steve", "O");
 player1.sayName(); // logs "steve"
 player2.sayName(); // logs "also steve"
+
+console.log(Object.getPrototypeOf(player1) === Player.prototype); // returns true
+console.log(Object.getPrototypeOf(player2) === Player.prototype); // returns true
+
+// Player.prototype.__proto__
+console.log(Object.getPrototypeOf(Player.prototype) === Object.prototype); // true
+// Output may slightly differ based on the browser
+console.log(player1.valueOf()); // Output: Object { name: "steve", marker: "X", sayName: sayName() }
+
+console.log(player1.hasOwnProperty("valueOf")); // false
+console.log(Object.prototype.hasOwnProperty("valueOf")); // true
+console.log(Object.prototype.hasOwnProperty("hasOwnProperty")); // true
+
+console.log(Object.getPrototypeOf(Object.prototype));
