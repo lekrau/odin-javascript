aa012570cd05b81ae466fd15cbfd428b3b406b12
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-02 08:20:47 +0200

    Move Book constructor exercise into separate file

--- object-constructors/script.js
+++ object-constructors/script.js
@@ -13,27 +13,3 @@ const player1 = new Player("steve", "X");
 const player2 = new Player("also steve", "O");
 player1.sayName(); // logs "steve"
 player2.sayName(); // logs "also steve"
-
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        if (this.read) {
-            return `${this.title} by ${this.author}, ${this.pages} pages, read`;
-        } else {
-            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
-        }
-    }
-}
-
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", false)
-console.log(theHobbit);
-console.log(theHobbit.info);
-console.log(theHobbit.info());
-theHobbit.read = true;
-console.log(theHobbit.info());
