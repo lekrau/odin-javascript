4dad18cacfcd33027334afced70fa5651ac133d1
lekrau <206306997+lekrau@users.noreply.github.com>
2026-08-15 08:13:16 +0200

    Add closure performance examples from MDN

--- factory-functions-and-the-module-pattern/mdn-closures/script.js
+++ factory-functions-and-the-module-pattern/mdn-closures/script.js
@@ -1,43 +1,29 @@
 "use strict";
 
-function showHelp(help) {
-    document.getElementById("help").textContent = help;
+function MyObject(name, message) {
+    this.name = name.toString();
+    this.message = message.toString();
+    // this.getName = function () {
+    //     return this.name;
+    // };
+
+    // this.getMessage = function () {
+    //     return this.message;
+    // };
 }
 
-// function makeHelpCallback(help) {
-//     return function () {
-//         showHelp(help);
-//     };
-// }
+// MyObject.prototype = {
+//     getName() {
+//         return this.name;
+//     },
+//     getMessage() {
+//         return this.message;
+//     },
+// };
 
-function setupHelp() {
-    const helpText = [
-        { id: "email", help: "Your email address" },
-        { id: "name", help: "Your full name" },
-        { id: "age", help: "Your age (you must be over 16)" },
-    ];
-
-    // for (let i = 0; i < helpText.length; i++) {
-    //     // (function () {
-    //     // Culprit is the use of `var` on this line
-    //     const item = helpText[i];
-    //     document.getElementById(item.id).onfocus =
-    //         function () { showHelp(item.help); };
-    //     // })(); // Immediate event listener attachment with the current value of item (preserved until iteration).
-    //     // makeHelpCallback(item.help);
-    // }
-
-    // for (const item of helpText) {
-    //     document.getElementById(item.id).onfocus = () => {
-    //         document.getElementById("help").textContent = item.help;
-    //     };
-    // }
-
-    helpText.forEach((item) => {
-        document.getElementById(item.id).onfocus = () => {
-            showHelp(item.help);
-        };
-    });
-}
-
-setupHelp();
+MyObject.prototype.getName = function () {
+    return this.name;
+};
+MyObject.prototype.getMessage = function () {
+    return this.message;
+};
